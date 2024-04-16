14773fbf09c48c057a31d15aaea2204fea386969
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-04-16 17:09:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+function check(){
+Answer = document.getElementById("pergunta").value;
+if(Answer == "Quem foi Thomas Edison?"){
+    document.getElementById("resposta").innerHTML = "Thomas Alva Edison foi um empresário dos Estados Unidos que patenteou e financiou o desenvolvimento de muitos dispositivos importantes de grande interesse industrial. 'O Feiticeiro de Menlo Park', como era conhecido, foi um dos primeiros a aplicar os princípios da produção maciça ao processo da invenção."
+}else{
+    document.getElementById("resposta").innerHTML = "Desculpe, não encontrei nenhuma resposta relacionada a sua pergunta, veja se perguntou certo, cometeu algum erro de ortografia, ou então tente perguntar de um outro jeito."
+}
+
+if(Answer == "Quem descobriu o Brasil?"){
+    document.getElementById("resposta").innerHTML = "Pedro Álvares Cabral, em 1500, fidalgo, comandante militar, navegador e explorador português."
+}
+
+if(Answer == "Qual a maior bilheteria da história?"){
+    document.getElementById("resposta").innerHTML = "Avatar(2009), arrecadando 2,923 bilhões USD em bilheteria."
+}
+
+if(Answer == "Qual o animal mais rápido do mundo?"){
+    document.getElementById("resposta").innerHTML = "Segundo Domenichelli, o animal terrestre mais veloz do mundo é o <b> guepardo </b>, também chamado de chita e parecido com o leopardo, que pode alcançar cerca de 115 km/h em poucos segundos."
+}
+
+if(Answer == "Quantos olhos tem uma abelha?"){
+    document.getElementById("resposta").innerHTML = "Uma abelha tem 5 olhos, dois deles ficam nas laterais, e os três menores ficam acima da cabeça, difíceis de ver."
+}
+
+if(Answer == "Quanto dinheiro vale cada kg de ouro?"){
+    document.getElementById("resposta").innerHTML = "1 kg de ouro é cerca de R$ 316 mil."
+}
+
+if(Answer == "É possível viajar no tempo?"){
+    document.getElementById("resposta").innerHTML = "A existência da viagem no tempo ainda é incerta, porém foram desenvolvidos alguns experimentos que te faz 'viajar no tempo'."
+}
+
+if(Answer == "O que é um íon?"){
+    document.getElementById("resposta").innerHTML = "É um átomo que ganha ou perde elétrons."
+}
+
+if(Answer == "Quantos reais vale 1 dólar?"){
+    document.getElementById("resposta").innerHTML = "1 dólar equivale a R$ 5,00 em 2024."
+}
+
+if(Answer == "Qual o filme com maior nota em imdb?"){
+    document.getElementById("resposta").innerHTML = "Um Sonho de Liberdade(1994), 9.3/10 em imdb."
+}
+
+if(Answer == "Quanto dinheiro tem o Elon Musk?"){
+    document.getElementById("resposta").innerHTML = "Elon Musk tem 196 bilhões USD(R$ 989,8 bilhões) em 2024."
+}
+
+if(Answer == "Quem foi o maior sniper da história?"){
+    document.getElementById("resposta").innerHTML = "Simo Hayha, mais conhecido como Morte Branca, um soldado irlandês, seu recorde foi um tiro de 457m."
+}
+
+if(Answer == "Quantos elementos tem na tabela periódica?"){
+    document.getElementById("resposta").innerHTML = "Na tabela periódica existem 118 elementos."
+}
+
+if(Answer == "O que é uma alcateia?"){
+    document.getElementById("resposta").innerHTML = "É um bando ou grupo de lobos."
+}
+
+if(Answer == "Qual o país mais populoso do mundo?"){
+    document.getElementById("resposta").innerHTML = "A Índia, com 1,4 bilhão de habitantes em 2024."
+}
+
+if(Answer == "Qual a fórmula da água?"){
+    document.getElementById("resposta").innerHTML = "A fórmula da água é H²O."
+}
+
+if(Answer == "Qual a fórmula de Bhaskara?"){
+    document.getElementById("resposta").innerHTML = "A fórmula de Bhaskara é : x = (-b±√(b²-4ac))/(2a) ."
+}
+
+if(Answer == "O que é o Oscar?"){
+    document.getElementById("resposta").innerHTML = "O Óscar ou Oscar oficialmente chamado de Prêmios da Academia ou Prémios da Academia, é uma cerimônia de premiação anual da Academia de Artes e Ciências Cinematográficas, fundado em Los Angeles em 1927, que presenteia anualmente os profissionais da indústria cinematográfica."
+}
+
+if(Answer == "Qual o primeiro robô fotográfico do mundo?"){
+    document.getElementById("resposta").innerHTML = "É o Selfiebot, um robô de selfie composto, principalmente, por uma luminária de escritório."
+}
+}
